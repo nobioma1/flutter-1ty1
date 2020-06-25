15eb0bdb6ecdf8ff58ce9ac985d6673e16677c03
Noble Obioma <nobioma1@gmail.com>
2020-06-25 19:31:37 +0100

    Use Stateless Widget

--- init101/lib/main.dart
+++ init101/lib/main.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('init101'),
-          centerTitle: true,
-          backgroundColor: Colors.teal[300],
-        ),
-        body: Center(
-          child: Text(
-            'Welcome to 101',
-            style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[600],
-                fontFamily: 'Lato'),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Text('Click'),
-          onPressed: () {},
-          backgroundColor: Colors.teal[300],
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('init101'),
+        centerTitle: true,
+        backgroundColor: Colors.teal[300],
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to init101 App!!',
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[600],
+              fontFamily: 'Lato'),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+        onPressed: () {},
+        backgroundColor: Colors.teal[300],
+      ),
+    );
+  }
+}
