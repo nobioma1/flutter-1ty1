d3406f0afea538e4049f7812c64b2e75aee31a1f
Noble Obioma <nobioma1@gmail.com>
2020-06-25 19:17:40 +0100

    Customize app Looks Font, Colors, Style

--- init101/lib/main.dart
+++ init101/lib/main.dart
@@ -5,12 +5,22 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(
           title: Text('init101'),
           centerTitle: true,
+          backgroundColor: Colors.teal[300],
         ),
         body: Center(
-          child: Text('Welcome to 101'),
+          child: Text(
+            'Welcome to 101',
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[600],
+                fontFamily: 'Lato'),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           child: Text('Click'),
+          onPressed: () {},
+          backgroundColor: Colors.teal[300],
         ),
       ),
     ));
