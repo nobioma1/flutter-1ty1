90a830c6a896723a01ee591ef0a64ed539c3681a
Noble Obioma <nobioma1@gmail.com>
2020-06-26 14:48:43 +0100

    Receive result asyncronously from popped screen

--- timery/lib/pages/home.dart
+++ timery/lib/pages/home.dart
@@ -10,7 +10,7 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context).settings.arguments;
+    data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
 
     String bgImage = data['isDayTime'] ? 'day.png' : 'night.png';
 
@@ -49,8 +49,17 @@ class _HomeState extends State<Home> {
                   height: 30,
                 ),
                 FlatButton.icon(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/location');
+                    onPressed: () async {
+                      dynamic result =
+                          await Navigator.pushNamed(context, '/location');
+                      setState(() {
+                        data = {
+                          'time': result['time'],
+                          'location': result['location'],
+                          'flag': result['time'],
+                          'isDayTime': result['isDayTime'],
+                        };
+                      });
                     },
                     icon: Icon(
                       Icons.edit_location,
