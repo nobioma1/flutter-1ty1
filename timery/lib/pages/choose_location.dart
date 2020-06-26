019911d391cc9bb39128be6be3cf79eebb1e03eb
Noble Obioma <nobioma1@gmail.com>
2020-06-26 14:48:06 +0100

    Pop out and pass data to current page

--- timery/lib/pages/choose_location.dart
+++ timery/lib/pages/choose_location.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:timery/locations.dart';
+import 'package:timery/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -6,6 +8,18 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void updateTime(index) async {
+    WorldTime worldTime = locations[index];
+    await worldTime.getTime();
+
+    Navigator.pop(context, {
+      'time': worldTime.time,
+      'location': worldTime.location,
+      'flag': worldTime.time,
+      'isDayTime': worldTime.isDayTime,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +30,21 @@ class _ChooseLocationState extends State<ChooseLocation> {
           centerTitle: true,
           elevation: 0,
         ),
-        body: Text('Choose Location Screen'));
+        body: ListView.builder(
+            itemCount: locations.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+                child: Card(
+                    child: ListTile(
+                        leading: Icon(Icons.flag),
+                        onTap: () {
+                          updateTime(index);
+                        },
+                        title: Text(locations[index]
+                            .location
+                            .replaceAll(new RegExp(r'-|_'), " ")))),
+              );
+            }));
   }
 }
