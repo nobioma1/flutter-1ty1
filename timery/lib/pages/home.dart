0b2c104d2de40cff0c4ba25ce10248aba33c7fd5
Noble Obioma <nobioma1@gmail.com>
2020-06-26 08:56:13 +0100

    Add widget for time and location

--- timery/lib/pages/home.dart
+++ timery/lib/pages/home.dart
@@ -13,17 +13,39 @@ class _HomeState extends State<Home> {
     data = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-      body: SafeArea(
-          child: Column(
-        children: <Widget>[
-          FlatButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.edit_location),
-              label: Text('Edit Location'))
-        ],
-      )),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+        child: SafeArea(
+            child: Column(
+          children: <Widget>[
+            FlatButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text('Edit Location')),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  data['location'],
+                  style: TextStyle(fontSize: 28.0, letterSpacing: 2),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              data['time'],
+              style: TextStyle(fontSize: 60.0, letterSpacing: 2),
+            )
+          ],
+        )),
+      ),
     );
   }
 }
