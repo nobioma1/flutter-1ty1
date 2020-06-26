00a87bf227b798f788520121cda9b634858e941f
Noble Obioma <nobioma1@gmail.com>
2020-06-26 08:37:12 +0100

    Type function to return Future<void> and fix string method error

--- timery/lib/services/world_time.dart
+++ timery/lib/services/world_time.dart
@@ -9,15 +9,15 @@ class WorldTime {
 
   WorldTime({this.location, this.flag, this.url});
 
-  void getTime() async {
+  Future<void> getTime() async {
     Response response = await get('http://worldtimeapi.org/api/timezone/$url');
     Map data = jsonDecode(response.body);
 
     String datetime = data['datetime'];
-    String offset = data['utc_offset'].subString(1, 3);
+    String offset = data['utc_offset'];
 
     DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+    now = now.add(Duration(hours: int.parse(offset.substring(1, 3))));
 
     // set time property
     time = now.toString();
