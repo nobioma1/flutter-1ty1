91f220d5c764d521f62a1218d2ab5b46216b35a9
Noble Obioma <nobioma1@gmail.com>
2020-06-26 09:31:38 +0100

    Set background image

--- timery/lib/pages/home.dart
+++ timery/lib/pages/home.dart
@@ -12,39 +12,55 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     data = ModalRoute.of(context).settings.arguments;
 
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-        child: SafeArea(
-            child: Column(
-          children: <Widget>[
-            FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location')),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    String bgImage = data['isDayTime'] ? 'day.png' : 'night.png';
+
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/$bgImage'), fit: BoxFit.cover)),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 100.0, 0, 0),
+            child: SafeArea(
+                child: Column(
               children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                          fontSize: 28.0,
+                          letterSpacing: 2,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
                 Text(
-                  data['location'],
-                  style: TextStyle(fontSize: 28.0, letterSpacing: 2),
-                )
+                  data['time'],
+                  style: TextStyle(
+                      fontSize: 60.0, letterSpacing: 2, color: Colors.white),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                FlatButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/location');
+                    },
+                    icon: Icon(
+                      Icons.edit_location,
+                      color: Colors.white,
+                    ),
+                    label: Text('Edit Location')),
               ],
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              data['time'],
-              style: TextStyle(fontSize: 60.0, letterSpacing: 2),
-            )
-          ],
-        )),
+            )),
+          ),
+        ),
       ),
     );
   }
